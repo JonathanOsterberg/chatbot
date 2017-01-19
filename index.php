47b8b4c17ee6fcb60232553d4bb4d3c61aebba91
JonathanOsterberg <Josterberg4141@outlook.com>
2017-01-19 12:45:06 +0200

    Skapade en startsida med formulär för namn
    chatbot

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to a trash bot</title>
+</head>
+<body>
+
+<label for="name">What's your name?</label>
+<input type="text" name="name" id="name=">
+<input type="submit" value="Send">
+
+</body>
+</html>
